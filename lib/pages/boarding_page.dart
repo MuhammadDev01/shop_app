4c8f3de3ab9ef1cb9dc1fb0b7acd48f82ea51add
Muhammad Khaled <muhammadbenkhaled@gmail.com>
2024-10-16 22:12:00 +0300

    change icon app and refactor boading page

--- lib/pages/boarding_page.dart
+++ lib/pages/boarding_page.dart
@@ -21,18 +21,21 @@ class BoardingPage extends StatefulWidget {
   const BoardingPage({super.key});
   final List<BoardingItemModel> boardingList = const [
     BoardingItemModel(
-      title: 'On Board 1 Title',
-      body: 'On Board 1 Body',
+      title: 'Shop What You Need Anytime',
+      body:
+          'Discover a wide variety of products to meet all your needs at competitive prices.',
       image: 'assets/images/shop1.png',
     ),
     BoardingItemModel(
-      title: 'On Board 2 Title',
-      body: 'On Board 2 Body',
+      title: 'Fast & Secure Delivery',
+      body:
+          'Get your orders delivered to your doorstep in no time with fast delivery services.',
       image: 'assets/images/shop2.png',
     ),
     BoardingItemModel(
-      title: 'On Board 3 Title',
-      body: 'On Board 3 Body',
+      title: 'Exclusive Deals & Special Discounts',
+      body:
+          'Enjoy special discounts and unbeatable offers on a wide range of products.',
       image: 'assets/images/shop3.png',
     ),
   ];
@@ -57,18 +60,23 @@ class _BoardingPageState extends State<BoardingPage> {
                   Navigator.pushAndRemoveUntil(
                     context,
                     MaterialPageRoute(
-                      builder: (context) =>  LoginPage(),
+                      builder: (context) => LoginPage(),
                     ),
                     (route) => false,
                   );
                 }
               },
-              child: const Text('SKIP'),
+              child: const Text(
+                'SKIP',
+                style: TextStyle(
+                  color: defaultColor,
+                ),
+              ),
             ),
           ],
         ),
         body: Padding(
-          padding: const EdgeInsets.all(30.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             children: [
               Expanded(
@@ -85,100 +93,114 @@ class _BoardingPageState extends State<BoardingPage> {
                       });
                     }
                   },
-                  physics: const BouncingScrollPhysics(),
                   itemBuilder: (context, index) => boardingBuildItem(
+                    context: context,
                     model: widget.boardingList[index],
                   ),
                   itemCount: 3,
                 ),
               ),
-              const SizedBox(
-                height: 60,
-              ),
-              Row(
-                children: [
-                  SmoothPageIndicator(
-                    controller: boardingController,
-                    count: widget.boardingList.length,
-                    effect: const ExpandingDotsEffect(
-                      dotColor: Colors.grey,
-                      dotHeight: 10,
-                      dotWidth: 10,
-                      activeDotColor: defaultColor,
-                      spacing: 5,
-                      expansionFactor: 4,
-                    ),
-                  ),
-                  const Spacer(),
-                  FloatingActionButton(
-                    onPressed: () async {
-                      if (isLast) {
-                        await CachedHelper.saveData(
-                            key: kOnBoarding, value: true);
-                        if (mounted) {
-                          Navigator.pushAndRemoveUntil(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) =>  LoginPage(),
-                            ),
-                            (route) => false,
-                          );
-                        }
-                      } else {
-                        boardingController.nextPage(
-                          duration: const Duration(
-                            milliseconds: 600,
-                          ),
-                          curve: Curves.fastLinearToSlowEaseIn,
-                        );
-                      }
-                    },
-                    child: const Icon(Icons.arrow_forward_ios),
-                  ),
-                ],
-              ),
+              _smoothPageIndicatorAndActionButton(),
             ],
           ),
         ));
+  }
+
+  Row _smoothPageIndicatorAndActionButton() {
+    return Row(
+      children: [
+        SmoothPageIndicator(
+          controller: boardingController,
+          count: widget.boardingList.length,
+          effect: const ExpandingDotsEffect(
+            dotColor: Colors.grey,
+            dotHeight: 10,
+            dotWidth: 10,
+            activeDotColor: defaultColor,
+            spacing: 5,
+            expansionFactor: 4,
+          ),
+        ),
+        const Spacer(),
+        FloatingActionButton(
+          backgroundColor: defaultColor,
+          foregroundColor: Colors.white,
+          onPressed: () async {
+            if (isLast) {
+              await CachedHelper.saveData(key: kOnBoarding, value: true);
+              if (mounted) {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => LoginPage(),
+                  ),
+                  (route) => false,
+                );
+              }
+            } else {
+              boardingController.nextPage(
+                duration: const Duration(
+                  milliseconds: 600,
+                ),
+                curve: Curves.fastLinearToSlowEaseIn,
+              );
+            }
+          },
+          child: const Icon(Icons.arrow_forward_ios),
+        ),
+      ],
+    );
   }
 }
 
 Widget boardingBuildItem({
   required BoardingItemModel model,
+  required BuildContext context,
 }) =>
     Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
+          flex: 3,
           child: Image.asset(
             model.image,
-            scale: 1,
+            height: 150,
           ),
         ),
-        const SizedBox(
-          height: 72,
-        ),
-        Text(
-          model.title,
-          style: const TextStyle(
-            fontSize: 24,
-            fontFamily: 'REM',
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(
-          height: 30,
-        ),
-        Text(
-          model.body,
-          style: const TextStyle(
-            fontSize: 14,
-            fontFamily: 'REM',
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(
-          height: 60,
+        _titleAndBodyBoadingPage(model, context),
+        SizedBox(
+          height: 20,
         ),
       ],
     );
+
+SizedBox _titleAndBodyBoadingPage(
+    BoardingItemModel model, BuildContext context) {
+  return SizedBox(
+    height: 150,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Text(
+            model.title,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        Expanded(
+          child: Text(
+            model.body,
+            style: const TextStyle(
+              fontSize: 14,
+              fontFamily: 'REM',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ],
+    ),
+  );
+}
